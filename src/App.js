2add2f877ead5b3e91fe590c3b4e7c4b568b3122
yassinehaimouch <haymouch53@gmail.com>
2023-04-28 23:01:20 +0100

    [Add]: Basic Styles

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import QuoteInfo from "./components/QuoteInfo";
 function App() {
   return (
     <div>
+      <div className="header">
+        <h2>Anime & Quotes</h2>
+        <h2>Api.</h2>
+      </div>
       <QuoteInfo />
     </div>
   );
